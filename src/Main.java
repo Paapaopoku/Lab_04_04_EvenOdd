9a8cd8041e2cab84725af12562f8e799cbb99c47
Hello world <Opokukka@mail.uc.edu>
2023-09-14 18:49:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+        int Modulo = 0;
+
+        System.out.println("The Modulo of " + numToExamine + " is " + Modulo);
+    }
+}
